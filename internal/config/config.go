96deb3700339d619e3a8d1a11de5f711e25075ac
Jens Fischer <jens-fischer@telekom.de>
2024-05-27 11:22:20 +0200

    feat(http): make read-buffer-size configurable

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,9 +6,10 @@ package config
 
 import (
 	"errors"
+	"strings"
+
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/viper"
-	"strings"
 )
 
 var LoadedConfiguration configuration
@@ -22,6 +23,7 @@ type configuration struct {
 	Port                            int      `mapstructure:"port"`
 	ResponseCode                    int      `mapstructure:"responseCode"`
 	MethodResponseCodeMapping       []string `mapstructure:"methodResponseCodeMapping"`
+	ReadBufferSize                  int      `mapstructure:"readBufferSize"`
 	StoreKeyRequestHeaders          []string `mapstructure:"storeKeyRequestHeaders"`
 	SlowlorisDefaultDurationSeconds int      `mapstructure:"slowlorisDefaultDurationSeconds"`
 	SlowlorisDefaultIntervalSeconds int      `mapstructure:"slowlorisDefaultIntervalSeconds"`
@@ -31,6 +33,7 @@ func setDefaults() {
 	viper.SetDefault("port", 8080)
 	viper.SetDefault("responseCode", 200)
 	viper.SetDefault("methodResponseCodeMapping", []string{})
+	viper.SetDefault("readBufferSize", 4096)
 	viper.SetDefault("storeKeyRequestHeaders", []string{"x-request-key"})
 	viper.SetDefault("slowlorisDefaultDurationSeconds", 15)
 	viper.SetDefault("slowlorisDefaultIntervalSeconds", 1)
